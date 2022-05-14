dd880a302712ab60e380d909ac547ba56763268f
PS3Xploit Team <esc0rtd3w@gmail.com>
2022-05-14 18:51:18 -0400

    3.1.0 Update Main: Added Support For 4.89

--- henplugin/main.c
+++ henplugin/main.c
@@ -334,6 +334,10 @@ static void downloadPKG_thread2(void)
 	else if(val==0x323032312F30342FULL)
 		{
 			download_interface->DownloadURL(0,(wchar_t *) L"http://ps3xploit.com/hen/release/488/cex/installer/Latest_HEN_Installer_signed.pkg", (wchar_t *) L"/dev_hdd0");
+		}		
+	else if(val==0x323032322F30322FULL)
+		{
+			download_interface->DownloadURL(0,(wchar_t *) L"http://ps3xploit.com/hen/release/489/cex/installer/Latest_HEN_Installer_signed.pkg", (wchar_t *) L"/dev_hdd0");
 		}	
 	thread2_download_finish=1;
 }
