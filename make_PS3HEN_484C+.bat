3205df3b27eed6096b9de16e7e7975e56c607215
PS3Xploit Team <esc0rtd3w@gmail.com>
2022-05-24 19:32:12 -0400

    Updated Batch Compiling Files
    thanks bguerville

--- make_PS3HEN_484C+.bat
+++ make_PS3HEN_484C+.bat
@@ -8,25 +8,25 @@ set CYGWIN=C:\PSDK3v2\MinGW\msys\1.0\bin
 
 mv *CEX_484 *CEX_485 *CEX_486 *CEX_487 *CEX_488 *CEX_489 PS3HEN_GEN
 cd henplugin
-%CYGWIN%\bash -i -c 'make debug; rm -r objs; rm henplugin.prx; rm henplugin.sym; mv henplugin.sprx ../PS3HEN_GEN;'
+%CYGWIN%\bash -i -c 'make release; rm -r objs; rm henplugin.prx; rm henplugin.sym; mv henplugin.sprx ../PS3HEN_GEN;'
 cd ../stage0_file
 rm -f *.o *.elf *.self *.bin *.map ../lv1/src/*.o ../debug/src/*.o ../lv2/src/*.o
 make -f Makefile all
 rm -f *.o *.elf *.self ../lv1/src/*.o ../debug/src/*.o ../lv2/src/*.o
 cd ../payload
 rm -f *.o *.elf *.self *.bin *.map ../lv1/src/*.o ../debug/src/*.o ../lv2/src/*.o
-make -f Makefile debug
+make -f Makefile release
 rm -f *.o *.elf *.self ../lv1/src/*.o ../debug/src/*.o ../lv2/src/*.o
 cd ..
 mv  *484C *485C *486C *487C *488C *489C PS3HEN_GEN
 cd PS3HEN_GEN
 gcc main.c -o HEN_GEN
-HEN_GEN PS3HEN.BIN_CEX_484 stage2.bin_debug_484C stage0.bin_484C HENplugin.sprx
-HEN_GEN PS3HEN.BIN_CEX_485 stage2.bin_debug_485C stage0.bin_485C HENplugin.sprx
-HEN_GEN PS3HEN.BIN_CEX_486 stage2.bin_debug_486C stage0.bin_486C HENplugin.sprx
-HEN_GEN PS3HEN.BIN_CEX_487 stage2.bin_debug_487C stage0.bin_487C HENplugin.sprx
-HEN_GEN PS3HEN.BIN_CEX_488 stage2.bin_debug_488C stage0.bin_488C HENplugin.sprx
-HEN_GEN PS3HEN.BIN_CEX_489 stage2.bin_debug_489C stage0.bin_489C HENplugin.sprx
+HEN_GEN PS3HEN.BIN_CEX_484 stage2.bin_484C stage0.bin_484C HENplugin.sprx
+HEN_GEN PS3HEN.BIN_CEX_485 stage2.bin_485C stage0.bin_485C HENplugin.sprx
+HEN_GEN PS3HEN.BIN_CEX_486 stage2.bin_486C stage0.bin_486C HENplugin.sprx
+HEN_GEN PS3HEN.BIN_CEX_487 stage2.bin_487C stage0.bin_487C HENplugin.sprx
+HEN_GEN PS3HEN.BIN_CEX_488 stage2.bin_488C stage0.bin_488C HENplugin.sprx
+HEN_GEN PS3HEN.BIN_CEX_489 stage2.bin_489C stage0.bin_489C HENplugin.sprx
 rm stage* *.sprx *.exe
-mv PS3HEN* ../
+mv PS3HEN.BIN_CEX_484 PS3HEN.BIN_CEX_485 PS3HEN.BIN_CEX_486 PS3HEN.BIN_CEX_487 PS3HEN.BIN_CEX_488 PS3HEN.BIN_CEX_489 ../
 pause
