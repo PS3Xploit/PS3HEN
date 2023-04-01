e6fd7a8be34869fe28c6839865fe6bc99dd2dbb3
PS3Xploit Team <esc0rtd3w@gmail.com>
2023-04-01 15:10:13 -0400

    Updated for 4.82 DEX

--- make_PS3HEN_debug.bat
+++ make_PS3HEN_debug.bat
@@ -6,7 +6,7 @@ set PS3DEV=%PS3SDK%/ps3dev2
 set PATH=%WIN_PS3SDK%/mingw/msys/1.0/bin;%WIN_PS3SDK%/mingw/bin;%PS3DEV%/ppu/bin;
 set CYGWIN=C:\PSDK3v2\MinGW\msys\1.0\bin
 
-mv *DEX_484_DEBUG *CEX_482_DEBUG *CEX_483_DEBUG *CEX_484_DEBUG *CEX_485_DEBUG *CEX_486_DEBUG *CEX_487_DEBUG *CEX_488_DEBUG *CEX_489_DEBUG *CEX_490_DEBUG PS3HEN_GEN
+mv *DEX_482_DEBUG *DEX_484_DEBUG *CEX_482_DEBUG *CEX_483_DEBUG *CEX_484_DEBUG *CEX_485_DEBUG *CEX_486_DEBUG *CEX_487_DEBUG *CEX_488_DEBUG *CEX_489_DEBUG *CEX_490_DEBUG PS3HEN_GEN
 cd henplugin
 %CYGWIN%\bash -i -c 'make debug; rm -r objs; rm henplugin.prx; rm henplugin.sym; mv henplugin.sprx ../PS3HEN_GEN;'
 cd ../stage0_file
@@ -18,9 +18,10 @@ rm -f *.o *.elf *.self *.bin *.map ../lv1/src/*.o ../debug/src/*.o ../lv2/src/*.
 make -f Makefile debug
 rm -f *.o *.elf *.self ../lv1/src/*.o ../debug/src/*.o ../lv2/src/*.o
 cd ..
-mv *484D *482C *483C *484C *485C *486C *487C *488C *489C *490C PS3HEN_GEN
+mv *482D *484D *482C *483C *484C *485C *486C *487C *488C *489C *490C PS3HEN_GEN
 cd PS3HEN_GEN
 gcc main.c -o HEN_GEN
+HEN_GEN PS3HEN.BIN_DEX_482_DEBUG stage2.bin_debug_482D stage0.bin_482D HENplugin.sprx
 HEN_GEN PS3HEN.BIN_CEX_482_DEBUG stage2.bin_debug_482C stage0.bin_482C HENplugin.sprx
 HEN_GEN PS3HEN.BIN_CEX_483_DEBUG stage2.bin_debug_483C stage0.bin_483C HENplugin.sprx
 HEN_GEN PS3HEN.BIN_DEX_484_DEBUG stage2.bin_debug_484D stage0.bin_484D HENplugin.sprx
