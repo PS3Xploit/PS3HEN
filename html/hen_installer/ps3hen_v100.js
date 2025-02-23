9f9bfdcf227ae5953731d101edfc6ccf87600057
Jason Borowski <esc0rtd3w@gmail.com>
2025-02-23 01:41:07 -0500

    Updated HTML/JS for 4.92

--- html/hen_installer/ps3hen_v100.js
+++ html/hen_installer/ps3hen_v100.js
@@ -859,6 +859,53 @@ var gadget_mod14_addr_491=0x633900; //load r3 dword
 var gadget_mod15_addr_491=0x39D038; //load r3 word
 var gadget_mod16_addr_491=0x4F732C; //set toc
 
+//CEX 4.92
+var toc_addr_492 = 0x6F5558;
+var default_vsh_pub_toc_492=0x6ED5AC;
+var vsh_opd_patch_492=0x96D5C;
+var vsh_opd_addr_492=0x6EBB70;
+var vsh_toc_addr_screenshot_492=0x72082C;// Updated From 4.83/4.84/4.85
+var vsh_ps3hen_key_toc_492=0x707A1C;// Updated From 4.83/4.84/4.85
+var toc_entry1_addr_492=0x6DA3D0;
+var toc_entry2_addr_492=0x725CE8;//idps Updated From 4.83/4.84/4.85 Double-check 4.92
+var toc_entry3_addr_492=0x6DA3C8;
+var toc_entry4_addr_492=0x740000;
+var toc_entry5_addr_492=0x6EB6C8;
+var toc_entry6_addr_492=0x0;
+var gadget1_addr_492=0x097604;
+var gadget2_addr_492=0x60A0E4;
+var gadget3_addr_492=0x0D505C;
+var gadget4_addr_492=0x229838;
+var gadget5_addr_492=0x12BB1C;
+var gadget6_addr_492=0x615CDC;//malloc
+var gadget7_addr_492=0x01FFD0;//memset
+var gadget8_addr_492=0x020000;//memcpy
+var gadget9_addr_492=0x029B08;
+var gadget10_addr_492=0x62E024;
+var gadget11_addr_492=0x59A4B0;// sprintf
+var gadget12_addr_492=0x0C864C;
+var gadget13_addr_492=0x48E5A8;//free
+var gadget14_addr_492=0x48C7A0;
+var gadget15_addr_492=0x489C88;
+var gadget_mod1_addr_492=0x60EFD8;
+var gadget_mod2_addr_492=0x013B74;
+var gadget_mod3_addr_492=0x0B8E00;
+var gadget_mod4a_addr_492=0x0D9684;
+var gadget_mod4b_addr_492=0x42C778;
+var gadget_mod4c_addr_492=0x054AF0; //load r5 word
+var gadget_mod5_addr_492=0x4238DC;
+var gadget_mod6_addr_492=0x020C00;
+var gadget_mod7_addr_492=0x01A6AC;
+var gadget_mod8_addr_492=0x2BACB8;// graceful exit
+var gadget_mod9_addr_492=0x010B20;
+var gadget_mod10_addr_492=0x1C5794;
+var gadget_mod11_addr_492=0x18B144;
+var gadget_mod12_addr_492=0x6331FC; //validation gadget
+var gadget_mod13_addr_492=0x336870; //store_r3 gadget
+var gadget_mod14_addr_492=0x633900; //load r3 dword
+var gadget_mod15_addr_492=0x39D038; //load r3 word
+var gadget_mod16_addr_492=0x4F732C; //set toc
+
 function hexh2bin(hex_val)
 {
 	return String.fromCharCode(hex_val);
@@ -2172,6 +2219,54 @@ function loadcex_491()
 	gadget_mod15_addr=gadget_mod15_addr_491;
 	gadget_mod16_addr=gadget_mod16_addr_491;
 }
+function loadcex_492()
+{
+	toc_addr = toc_addr_492;
+	vsh_opd_addr=vsh_opd_addr_492;
+	vsh_opd_patch=vsh_opd_patch_492;
+	vsh_toc_addr_screenshot=vsh_toc_addr_screenshot_492;
+	vsh_ps3hen_key_toc=vsh_ps3hen_key_toc_492;
+	default_vsh_pub_toc=default_vsh_pub_toc_492;
+	toc_entry1_addr=toc_entry1_addr_492;
+	toc_entry2_addr=toc_entry2_addr_492;
+	toc_entry3_addr=toc_entry3_addr_492;
+	toc_entry4_addr=toc_entry4_addr_492;
+	toc_entry5_addr=toc_entry5_addr_492;
+	toc_entry6_addr=toc_entry6_addr_492;
+	gadget1_addr=gadget1_addr_492;
+	gadget2_addr=gadget2_addr_492;
+	gadget3_addr=gadget3_addr_492;
+	gadget4_addr=gadget4_addr_492;
+	gadget5_addr=gadget5_addr_492;
+	gadget6_addr=gadget6_addr_492;
+	gadget7_addr=gadget7_addr_492;
+	gadget8_addr=gadget8_addr_492;
+	gadget9_addr=gadget9_addr_492;
+	gadget10_addr=gadget10_addr_492;
+	gadget11_addr=gadget11_addr_492;
+	gadget12_addr=gadget12_addr_492;
+	gadget13_addr=gadget13_addr_492;
+	gadget14_addr=gadget14_addr_492;
+	gadget15_addr=gadget15_addr_492;
+	gadget_mod1_addr=gadget_mod1_addr_492;
+	gadget_mod2_addr=gadget_mod2_addr_492;
+	gadget_mod3_addr=gadget_mod3_addr_492;
+	gadget_mod4a_addr=gadget_mod4a_addr_492;
+	gadget_mod4b_addr=gadget_mod4b_addr_492;
+	gadget_mod4c_addr=gadget_mod4c_addr_492;
+	gadget_mod5_addr=gadget_mod5_addr_492;
+	gadget_mod6_addr=gadget_mod6_addr_492;
+	gadget_mod7_addr=gadget_mod7_addr_492;
+	gadget_mod8_addr=gadget_mod8_addr_492;
+	gadget_mod9_addr=gadget_mod9_addr_492;
+	gadget_mod10_addr=gadget_mod10_addr_492;
+	gadget_mod11_addr=gadget_mod11_addr_492;
+	gadget_mod12_addr=gadget_mod12_addr_492;
+	gadget_mod13_addr=gadget_mod13_addr_492;
+	gadget_mod14_addr=gadget_mod14_addr_492;
+	gadget_mod15_addr=gadget_mod15_addr_492;
+	gadget_mod16_addr=gadget_mod16_addr_492;
+}
 /*
 function dex()
 {
@@ -2283,6 +2378,13 @@ function dex()
 			//if(document.getElementById('dex').checked===true){loaddex_491();}//alert("calling loaddex_491");
 			//else {loadcex_491();}
 			//loadcex_491();
+			//disable_trigger();
+			break;
+			
+		case "4.92":
+			//if(document.getElementById('dex').checked===true){loaddex_492();}//alert("calling loaddex_492");
+			//else {loadcex_492();}
+			//loadcex_492();
 			//disable_trigger();
 			break;
 		
@@ -2623,7 +2725,7 @@ function fill_by_16bytes(nbytes,hex_val)
 //########################## End ROP Framework functions by bguerville(under development) #########################
 function ps3chk(){
 
-	var fwCompat = ["4.00","4.10","4.11","4.20","4.21","4.25","4.30","4.31","4.40","4.41","4.45","4.46","4.50","4.53","4.55","4.60","4.65","4.66","4.70","4.75","4.76","4.78","4.80","4.81","4.82","4.83","4.84","4.85","4.86","4.87","4.88","4.89","4.90","4.91"];
+	var fwCompat = ["4.00","4.10","4.11","4.20","4.21","4.25","4.30","4.31","4.40","4.41","4.45","4.46","4.50","4.53","4.55","4.60","4.65","4.66","4.70","4.75","4.76","4.78","4.80","4.81","4.82","4.83","4.84","4.85","4.86","4.87","4.88","4.89","4.90","4.91","4.92"];
 	var ua = navigator.userAgent;
 	var uaStringCheck = ua.substring(ua.indexOf("5.0 (") + 5, ua.indexOf(") Apple") - 7);
 	var fwVersion = ua.substring(ua.indexOf("5.0 (") + 19, ua.indexOf(") Apple"));
@@ -2702,10 +2804,16 @@ function ps3chk(){
 					//alert(msgHFW);
 //					initDEX();
 					loadcex_491();
+					break;
+					
+				case fwCompat[34]:
+					//alert(msgHFW);
+//					initDEX();
+					loadcex_492();
 					break;					
 					
 				default:
-					alert('Your PS3 is not on FW 4.80 - 4.91! Your current running FW version is ' + fwVersion + ', which is not compatible with PS3HEN. All features have been disabled');
+					alert('Your PS3 is not on FW 4.80 - 4.92! Your current running FW version is ' + fwVersion + ', which is not compatible with PS3HEN. All features have been disabled');
 					disable_all();
 					break;
 			}
