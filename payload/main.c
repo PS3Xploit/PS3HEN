6153dcec0d52ae7d0aab8214230eabd3be07cd33
Jason Borowski <esc0rtd3w@gmail.com>
2025-03-06 02:04:32 -0500

    Updated HEN_REV to 3.4.0 for added 4.92 support

--- payload/main.c
+++ payload/main.c
@@ -61,7 +61,7 @@
 
 #define COBRA_VERSION		0x0F
 #define COBRA_VERSION_BCD	0x0840
-#define HEN_REV				0x0331
+#define HEN_REV				0x0340
 
 #if defined(FIRMWARE_4_80)
 	#define FIRMWARE_VERSION	0x0480
